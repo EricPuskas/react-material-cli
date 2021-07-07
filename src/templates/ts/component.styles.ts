eddfd6c00ef10fd44cbfb7b85c976c3b7dadd11e
Eric Puskas <eric.puskas@yahoo.com>
2021-07-07 17:54:31 +0300

    #1: Test creating boilerplate

---
+++ src/templates/ts/component.styles.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+/**
+ *  Material UI Imports
+ * @see https://material-ui.com/styles/basics/
+ */
+import { makeStyles, Theme } from "@material-ui/core/styles";
+
+/**
+ * Styles the component
+ */
+const useStyles = makeStyles((theme: Theme) => ({
+  COMPONENT_NAME: {}
+}));
+
+export { useStyles };
